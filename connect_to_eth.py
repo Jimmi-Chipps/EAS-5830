a221559edacf68c6435f891116ec91ee2273f3d1
Jordan Steen <76076478+Jimmi-Chipps@users.noreply.github.com>
2024-10-21 20:57:40 -0700

    Add files via upload

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -28,7 +28,7 @@ def connect_with_middleware(contract_json):
 
 	# TODO complete this method
 	# The first section will be the same as "connect_to_eth()" but with a BNB url
-	url = "https://sepolia.infura.io/v3/c90e861ee43b4b41be927f80f2e012bc"
+	url = "https://bsc-testnet.infura.io/v3/c90e861ee43b4b41be927f80f2e012bc"
 	w3 = Web3(HTTPProvider(url))
 	assert w3.is_connected(), f"Failed to connect to provider at {url}"
 
@@ -40,10 +40,6 @@ def connect_with_middleware(contract_json):
 
 	contract = w3.eth.contract(abi=abi, address=address)
 	return w3, contract
-
-w3=connect_to_eth()
-w3.is_connected()
-w3.eth.get_block('latest')
 
 if __name__ == "__main__":
 	connect_to_eth()
