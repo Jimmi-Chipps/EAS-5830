3a6a609e2f8032f08fa3956d9a2abb81539793cc
Jordan Steen <76076478+Jimmi-Chipps@users.noreply.github.com>
2026-02-27 17:53:32 -0800

    Update connect_to_eth.py

--- connect_to_eth.py
+++ connect_to_eth.py
@@ -4,14 +4,6 @@ from web3 import Web3
 from web3.middleware import ExtraDataToPOAMiddleware
 from web3.providers.rpc import HTTPProvider
 
-'''If you use one of the suggested infrastructure providers, the url will be of the form
-now_url  = f"https://eth.nownodes.io/{now_token}"
-alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
-infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
-'''
-
-
-
 def connect_to_eth():
 	url = "https://mainnet.infura.io/v3/c90e861ee43b4b41be927f80f2e012bc"
 	w3 = Web3(HTTPProvider(url))
@@ -36,11 +28,21 @@ def connect_with_middleware(contract_json):
 	# The second section requires you to inject middleware into your w3 object and
 	# create a contract object. Read more on the docs pages at https://web3py.readthedocs.io/en/stable/middleware.html
 	# and https://web3py.readthedocs.io/en/stable/web3.contract.html
-  # Inject the Geth PoA middleware
+  	
 	w3.middleware_onion.inject(ExtraDataToPOAMiddleware, layer=0)
 
 	contract = w3.eth.contract(abi=abi, address=address)
 	return w3, contract
 
 if __name__ == "__main__":
-	connect_to_eth()
+
+	# w3 = connect_to_eth()
+	# print("Connected:", w3.is_connected())
+	# print(w3.eth.block_number)
+
+	w3, contract = connect_with_middleware('contract_info.json')
+	
+	print("Connected:", w3.is_connected())
+	print("Chain ID:", w3.eth.chain_id)
+	print("Latest block:", w3.eth.block_number)
+	print("Contract address:", contract.address)
