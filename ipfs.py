d2fc9eda9b436ac70505a7e16c8f85c2272f7dce
Jordan Steen <76076478+Jimmi-Chipps@users.noreply.github.com>
2026-03-01 15:49:22 -0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,14 +1,6 @@
-import json
 from web3 import Web3
-from web3.middleware import geth_poa_middleware
+# from web3.middleware import geth_poa_middleware
 from web3.providers.rpc import HTTPProvider
-
-'''If you use one of the suggested infrastructure providers, the url will be of the form
-now_url  = f"https://eth.nownodes.io/{now_token}"
-alchemy_url = f"https://eth-mainnet.alchemyapi.io/v2/{alchemy_token}"
-infura_url = f"https://mainnet.infura.io/v3/{infura_token}"
-'''
-
 import requests
 import json
 
@@ -23,17 +15,15 @@ def pin_to_ipfs(data):
 
 def get_from_ipfs(cid, content_type="json"):
     assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
-    
-    # Send a GET request to retrieve the data from IPFS
+    # get request
     response = requests.post("http://127.0.0.1:5001/api/v0/cat", params={"arg": cid})
 
     
     if response.status_code == 200:
-        # Parse the response content based on the expected content type
         if content_type == "json":
             data = json.loads(response.text)
         else:
-            data = response.text  # Return as text if not JSON
+            data = response.text
             
         assert isinstance(data, dict), f"get_from_ipfs should return a dict"
         return data
@@ -41,4 +31,5 @@ def get_from_ipfs(cid, content_type="json"):
         raise Exception(f"Error fetching data from IPFS: {response.text}")
     
 cid=pin_to_ipfs({'I saw' : 'water', "matban":"tancan"})
+print(cid)
 print(get_from_ipfs(cid))
