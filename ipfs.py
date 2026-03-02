9bb6937d0fcb0055a99fc754f71723137d3f2955
Jordan Steen <76076478+Jimmi-Chipps@users.noreply.github.com>
2026-03-01 16:43:55 -0800

    Update ipfs.py

--- ipfs.py
+++ ipfs.py
@@ -1,35 +1,37 @@
-from web3 import Web3
-# from web3.middleware import geth_poa_middleware
-from web3.providers.rpc import HTTPProvider
 import requests
 import json
 
+api_key = '3f5419954a54faf99fed'
+secret_key = '0e228df580ce9b2762489db1cca3dd89cca53456259dd11fab64cf9d0f33ebfd'
+
 def pin_to_ipfs(data):
     assert isinstance(data, dict), f"Error pin_to_ipfs expects a dictionary"
-    json_data = json.dumps(data)
-    post = requests.post("http://127.0.0.1:5001/api/v0/add", files={"file": json_data})
-
-    if post.status_code == 200:
-        cid = post.json().get("Hash")
-        return cid
+    pinata_url = "https://api.pinata.cloud/pinning/pinJSONToIPFS"
+    headers =  {
+        "Content-Type": "application/json",
+        "pinata_api_key": api_key,
+        "pinata_secret_api_key": secret_key,
+    }
+    #json_data = json.dumps(data)
+    post = requests.post(pinata_url, headers= headers, json=data)
+    
+    #Errors
+    print("status:", post.status_code)
+    print("text:", post.text)
+    print("json:", post.json())
+    return post.json()["IpfsHash"]
 
 def get_from_ipfs(cid, content_type="json"):
     assert isinstance(cid, str), f"get_from_ipfs accepts a cid in the form of a string"
-    # get request
-    response = requests.post("http://127.0.0.1:5001/api/v0/cat", params={"arg": cid})
+    
+    # Send a GET request to retrieve the data from IPFS
+    pinata_url = f"https://gateway.pinata.cloud/ipfs/{cid}"
+    response = requests.get(pinata_url)
+    
+    return response.json()
+    
+if __name__ == "__main__":
 
-    
-    if response.status_code == 200:
-        if content_type == "json":
-            data = json.loads(response.text)
-        else:
-            data = response.text
-            
-        assert isinstance(data, dict), f"get_from_ipfs should return a dict"
-        return data
-    else:
-        raise Exception(f"Error fetching data from IPFS: {response.text}")
-    
-cid=pin_to_ipfs({'I saw' : 'water', "matban":"tancan"})
-print(cid)
-print(get_from_ipfs(cid))
+    cid=pin_to_ipfs({'I saw' : 'water', "matban":"tancan"})
+    print(cid)
+    print(get_from_ipfs(cid))
